f08c113b0c4b2f35d9522c84ee2b2358a553870c
Daryyya <darya9841402670@gmail.com>
2022-10-29 14:25:46 +0300

    feat: add styled component

---
+++ styles/home.ts
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled'
+import Image from 'next/image'
+
+export const Main = styled.main`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+`
+export const Bg = styled(Image)`
+  object-fit: cover;
+  object-position: center;
+`
